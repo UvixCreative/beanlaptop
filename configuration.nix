e89b3abe0f7935e197b47b58bd4589e58c2c0deb
Ben Rankin <me@benrank.in>
2024-04-27 11:10:39 -0400

    Fixed xkb variable rename warnings

--- configuration.nix
+++ configuration.nix
@@ -72,9 +72,9 @@
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable numlock at login
