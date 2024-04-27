afde97691aaaa087e759218e4686031ef625786c
Ben Rankin <me@benrank.in>
2024-04-27 10:08:19 -0400

    Maybe fixed overlay

--- swayfx-overlay.nix
+++ swayfx-overlay.nix
@@ -1,14 +1,23 @@
-{config, pkgs, lib, ...}: {		nixpkgs.overlays = [
-			(self: super:
-			{
-				swayfx = super.swayfx.overrideAttrs (oldArrts: rec {
-					version = "a5a69d";
-					src = super.fetchFromGitHub {
-						owner = "WillPower3309";
-						repo = "swayfx";
-						rev = "a5a69d4d4e9a8fe306e27ca46cde3a8b9d312ae3";
-						sha256 = "sha256-aQVHGi6oqceTlyzzzZnErVHYDJbDfG0sdWe+ItuH8a4=";
-					};
-				});
-			})
-		];}
+({ config, pkgs, ... }: {
+  nixpkgs.overlays = let
+    version = "2024-04-27";
+  in [(
+    final: prev: {
+      swayfx-unwrapped = prev.swayfx.overrideAttrs (oldAttrs: {
+        inherit version;
+        src = super.fetchFromGitHub {
+          owner = "WillPower3309";
+          repo = "swayfx";
+          rev = "a5a69d4d4e9a8fe306e27ca46cde3a8b9d312ae3";
+          sha256 = "sha256-aQVHGi6oqceTlyzzzZnErVHYDJbDfG0sdWe+ItuH8a4=";
+        };
+      });
+
+      swayfx = (prev.swayfx.override (oldArgs: {
+        swayfx-unwrapped = final.swayfx-unwrapped;
+      })).overrideAttrs (oldAttrs: {
+        inherit version;
+      });
+    }
+  )];
+})
