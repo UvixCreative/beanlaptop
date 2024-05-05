91bd8d271cb8a238f11812f3810d3203a03bb42e
UvixCreative <me@benrank.in>
2024-05-05 11:29:22 -0400

    Reorganization, added categories delimited by ==

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,7 @@
 { config, pkgs, lib, ... }:
 
 {
+  # == NixOS things ==
   imports =
     [
       ./home-manager/home-manager.nix
@@ -12,17 +13,38 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Bootloader.
+  # == Basic system/hardware config ==
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable and configure openGL
+  hardware.opengl = {
+    enable = true;
+    driSupport.enable = true;
+    extraPackages = with pkgs; [
+      vaapiVdpau
+      libvdpau-va-gl
+      amdvlk
+    ];
+  };
+
+  # systemd-boot bootloader
   boot.loader.systemd-boot.enable = true;
-
-  # Polkit
-  security.polkit.enable = true;
-
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -45,6 +67,27 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  # == Desktop/WM config ==
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable SDDM and the KDE Plasma Desktop Environment
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Enable numlock at login
+  services.xserver.displayManager.sddm.autoNumlock = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
   # Enable Sway WM
   programs.sway = {
     enable = true;
@@ -54,65 +97,13 @@
     };
   };
 
-  hardware.opengl = {
-    enable = true;
-    driSupport.enable = true;
-    extraPackages = with pkgs; [
-      vaapiVdpau
-      libvdpau-va-gl
-      amdvlk
-    ];
-  };
-
+  # Program to configure screen brightness
   programs.light.enable = true;
 
-  programs.steam = {
-    remotePlay.openFirewall = true;
-  };
+  # == User accounts and packages ==
 
-  services.flatpak.enable = true;
+  # ++ Users ++
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  programs.ssh.askPassword = lib.mkForce "${pkgs.x11_ssh_askpass}/libexec/x11-ssh-askpass";
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable numlock at login
-  services.xserver.displayManager.sddm.autoNumlock = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.bean = {
     isNormalUser = true;
     description = "Ben Rankin";
@@ -122,18 +113,22 @@
     shell = pkgs.fish;
   };
 
+  # ++ Packages ++
+
   # Allow unfree packages
   nixpkgs.config = {
     allowUnfree = true;
     chromium = {
-      enableWideVine = true;
+      enableWideVine = true; # Enable DRM in Chromium
     };
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # System nixos packages
+  programs.kdeconnect.enable = true;
+  programs.fish.enable = true;
+
+  # System nixpkgs packages
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      firefox
      kitty
      discord
@@ -159,13 +154,19 @@
      chromium
   ];
 
+  # Flatpak packages
+  services.flatpak.enable = true;
   services.flatpak.packages = [
     "app.bluebubbles.BlueBubbles"
     "com.valvesoftware.Steam"
   ];
 
-  programs.fish.enable = true;
-  programs.kdeconnect.enable = true;
+  # Steam is installed as flatpak, so keep nixos steam disabled, but open remoteplay firewall
+  programs.steam = {
+    remotePlay.openFirewall = true;
+  };
+
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -174,18 +175,25 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
+  # == Services ==
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Polkit
+  security.polkit.enable = true;
 
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
     ports = [ 1803 ];
   };
+
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 1803 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+
+  # == State version thing don't worry about it ==
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
