b1c72796ec15d75a6c5427bd2f1923f46dd8d834
UvixCreative <me@benrank.in>
2024-05-18 13:16:56 -0400

    Sway bindgesture config

--- home-manager/bean/home.nix
+++ home-manager/bean/home.nix
@@ -19,6 +19,18 @@
     config = import ./sway.nix;
     extraConfig = ''
 title_align center
+bindgesture swipe:3:right workspace next
+bindgesture swipe:3:left workspace prev
+bindgesture swipe:3:down exec light -U 10
+bindgesture swipe:3:up exec light -A 10
+bindgesture swipe:4:up focus up
+bindgesture swipe:4:down focus down
+bindgesture swipe:4:right focus right
+bindgesture swipe:4:left focus left
+bindgesture pinch:2:inward+up move up
+bindgesture pinch:2:inward+down move down
+bindgesture pinch:2:inward+left move left
+bindgesture pinch:2:inward+right move right
 '';
   };
 
